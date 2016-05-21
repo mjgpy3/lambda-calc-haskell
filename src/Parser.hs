b34d6e2afebf8b2bda215d7500a8ab2c37811159
Michael Gilliland <mjg.py3@gmail.com>
2016-05-21 14:21:21 -0400

    Small change, create overFirst, normally a bifunctor function

--- src/Parser.hs
+++ src/Parser.hs
@@ -9,13 +9,13 @@ parse = fst . parseSingle
 parseSingle :: [T.Token] -> (A.Ast, [T.Token])
 parseSingle (T.Name name:rest) = (A.Name name, rest)
 parseSingle (T.Lambda:T.Name arg:T.Dot:rest) =
-  let
-    (body, rest2) = parseSingle rest
-  in
-    (A.Lambda arg body, rest2)
+  overFirst (A.Lambda arg) $ parseSingle rest
 parseSingle (T.LParen:rest) =
   let
     (func, rest1) = parseSingle rest
     (arg, _:rest2) = parseSingle rest1
   in
     (A.Application func arg, rest2)
+
+overFirst :: (a -> b) -> (a, c) -> (b, c)
+overFirst fn (x, y) = (fn x, y)
